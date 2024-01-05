7036bed5465646baf0d950d96079b6c03bb14704
Kirill108 <kirill108bol@gmail.com>
2024-01-05 11:56:54 +0100

    fix: add cors politice

---
+++ capybara/src/config/cors.config.ts
@@ -0,0 +1,5 @@
+export const CorsConfig = {
+  origin: ['https://capybara-sand.vercel.app'],
+  methods: 'GET,PUT,PATCH,POST,DELETE',
+  allowedHeaders: ['Content-Type'],
+};
--- capybara/src/main.ts
+++ capybara/src/main.ts
@@ -2,9 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule } from '@nestjs/swagger';
 import { swaggerConfig } from './config/swagger.config';
+import { CorsConfig } from './config/cors.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors(CorsConfig);
 
   const document = SwaggerModule.createDocument(app, swaggerConfig);
   SwaggerModule.setup('/docs', app, document);
